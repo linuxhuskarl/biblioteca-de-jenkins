56c478decef6c0c7e3e9af983b5e78bbca9f0f10
Maciej Brzeski <linuxhuskarl@gmail.com>
2022-01-25 11:36:35 +0100

    adding some files

---
+++ vars/foo.groovy
@@ -0,0 +1,14 @@
+// vars/foo.groovy
+def info (message) {
+    echo "INFO: ${message}"
+}
+
+def warning (message) {
+    echo "WARN: ${message}"
+}
+
+def error (message) {
+    echo "ERROR: ${message}"
+}
+
+def secret = "Don't tell anyone"
